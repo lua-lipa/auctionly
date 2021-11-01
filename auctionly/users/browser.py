d32d5f6d9ef15267f54385617659b5ddb24b690a
eronaaliu <50829986+eronaaliu@users.noreply.github.com>
2021-11-01 13:21:42 +0000

    removed some unnecessary changes in browser

--- auctionly/users/browser.py
+++ auctionly/users/browser.py
@@ -1,8 +1,5 @@
 import user
 
-# As a browser I want to be able to view the art that is available on the website so that I don't have to create an account unless I'm interested.
-#As a browser I want to be able to create an account if I want to so that I can buy/sell art.#
-
 
 class browser(user):
     def __init__(self, user_id, name, last_name, email, password):
@@ -17,9 +14,3 @@ class browser(user):
 
     def add_art_displayed_on_feed(self, art):
         self.art_displayed_on_feed.append(art)
-
-    # def create_account(self, user)
-
-    # create account
-    # check if user is logged in
-    # let them apply to purchase an account
