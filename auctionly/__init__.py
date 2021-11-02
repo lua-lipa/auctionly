119d5f6f429eb9a4d97391af4b9f32f7ac318ee0
nutsachichilidze <36604389+nutsachichilidze@users.noreply.github.com>
2021-11-02 17:55:05 +0000

    resolving conflicts with my branch

--- auctionly/__init__.py
+++ auctionly/__init__.py
@@ -1,10 +1,21 @@
-from auctionly.auction.dutchAuction import DutchAuction
+from flask import Flask
 from users import seller
 from users import buyer 
 from art import art 
-from auction import dutchAuction
-def mock_auction_process(): 
-    # set up seller and buyer
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'alkjsfkalalfks'
+
+    from .views import views
+    from .auth import auth
+    
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    return app
+
+def mock_users(): 
     seller_a = seller(user_id="1", 
                       name="Nutsa", 
                       last_name="chichilidze", 
@@ -19,8 +30,7 @@ def mock_auction_process():
 
     seller_art_types = ["abstract", "modern"]
     seller_a.set_art_types(seller_art_types)
-    
-    # add art for auction
+
     art_to_upload = art(art_id="1", 
                         owner_id=seller_a.get_user_id(), 
                         digital_image_path="art.jpg", 
@@ -28,11 +38,8 @@ def mock_auction_process():
                         art_category="modern")
 
     seller.add_for_exhibition(art_to_upload)
-    auction = DutchAuction()
-    auction.set_start_time() 
-    auction.set_bid_increment()
 
+    # mock auction
     # mock bids 
 
     
-
