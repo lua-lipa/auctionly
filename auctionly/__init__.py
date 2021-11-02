58dfd680bd9178759b094ab9433e030df31fc147
nutsachichilidze <36604389+nutsachichilidze@users.noreply.github.com>
2021-11-02 13:19:39 +0000

    mocking the auction process

---
+++ auctionly/__init__.py
@@ -0,0 +1,33 @@
+from users import seller
+from users import buyer 
+from art import art 
+
+def mock_users(): 
+    seller_a = seller(user_id="1", 
+                      name="Nutsa", 
+                      last_name="chichilidze", 
+                      email="nchichilidze@gmail.com", 
+                      password="1234")
+
+    buyer_a = buyer(user_id="2", 
+                    name="Ranya", 
+                    last_name="ElHwigi", 
+                    email="ranya@gmail.com", 
+                    password="1234")
+
+    seller_art_types = ["abstract", "modern"]
+    seller_a.set_art_types(seller_art_types)
+
+    art_to_upload = art(art_id="1", 
+                        owner_id=seller_a.get_user_id(), 
+                        digital_image_path="art.jpg", 
+                        description="art", 
+                        art_category="modern")
+
+    seller.add_for_exhibition(art_to_upload)
+
+    # mock auction
+    # mock bids 
+
+    
+
