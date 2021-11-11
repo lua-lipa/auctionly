34b989bb9d810ff91639d4508c212ad970f8b275
eronaaliu <50829986+eronaaliu@users.noreply.github.com>
2021-11-11 20:29:54 +0000

    profile page added

--- auctionly/views.py
+++ auctionly/views.py
@@ -3,7 +3,14 @@ from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
 
+
 @views.route('/')
 @login_required
 def home():
     return render_template("home.html")
+
+
+@views.route('/profile')
+@login_required
+def profile():
+    return render_template("profile.html")
