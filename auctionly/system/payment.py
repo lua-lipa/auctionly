1c6790e738a36616117464df7815329c1fb5d14a
nutsachichilidze <36604389+nutsachichilidze@users.noreply.github.com>
2021-11-15 19:54:39 +0000

    the payment system checks if bidding is allowed

---
+++ auctionly/system/payment.py
@@ -0,0 +1,7 @@
+class Payment():
+
+    def receive_bid_from_user(user_id, amount):
+        # check if user has already placed bid bid = bid - prev_bid
+        # freeze money
+        print("Succesfully received the bid from the user")
+        return True
