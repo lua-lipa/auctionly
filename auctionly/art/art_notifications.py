a8e951dfff3e747ba591407e8c57cb4447d77e0e
Ranya Hwigi <18227449@studentmail.ul.ie>
2021-11-26 16:20:58 +0000

    added comments to art notifications class

--- auctionly/art/art_notifications.py
+++ auctionly/art/art_notifications.py
@@ -7,8 +7,10 @@ class Art_Notifications(db.Model):
     art_id = db.Column(db.Integer, db.ForeignKey("art.id"))
 
     def __init__(self, user_id, art_id):
+        """creates an art notification object/row in the table"""
         self.user_id = user_id
         self.art_id = art_id
 
     def get_art_id(self):
+        """returns the art id of a row in a table"""
         return self.art_id
