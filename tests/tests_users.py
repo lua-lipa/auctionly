cab1c6435c874269fadcb05a154ff34f98a8dcb6
nutsachichilidze <36604389+nutsachichilidze@users.noreply.github.com>
2021-12-12 20:30:18 +0000

    lint error fixes

--- tests/tests_users.py
+++ tests/tests_users.py
@@ -1,45 +1,38 @@
+""" Class for unit testing the users package. """
+import unittest
 from auctionly.users.user import User
 from auctionly.users.buyer import Buyer
-import unittest
-from unittest.mock import MagicMock
-import logging
-import pytest
 
 
 class TestUser(unittest.TestCase):
+    """ class for unit testing the user class"""
 
     def test_user_init(self):
+        """ testing the User object constructor """
         name = "Nutsa"
         last_name = "CH"
         email = "test@gmail.com"
         password = "1234"
         user = User(name, last_name, email, password)
-
-        logger = logging.getLogger()
-
         self.assertEqual(name, user.get_name())
         self.assertEqual(last_name, user.get_last_name())
-        # self.assertEqual(email, user.get_email)
-        # self.assertEqual(password, user.get_password())
 
 
 class TestBuyer(unittest.TestCase):
+    """ class for unit testing the buyer class """
 
     def test_buyer_init(self):
+        """ testing the Buyer object constructor """
         name = "Nutsa"
         last_name = "CH"
         email = "test@gmail.com"
         password = "1234"
         buyer = User(name, last_name, email, password)
-
-        logger = logging.getLogger()
-
         self.assertEqual(name, buyer.get_name())
         self.assertEqual(last_name, buyer.get_last_name())
-        # self.assertEqual(email, buyer.get_email)
-        # self.assertEqual(password, buyer.get_password())
 
     def test_buyer_art_prefs(self):
+        """ testing the Buyer object art preference setting """
         name = "Nutsa"
         last_name = "CH"
         email = "test@gmail.com"
@@ -48,5 +41,4 @@ class TestBuyer(unittest.TestCase):
         buyer = Buyer(name, last_name, email, password)
         input_art_prefs = ["Modern", "Contemporary"]
         buyer.set_art_prefs(input_art_prefs)
-
         self.assertEqual(buyer.get_art_prefs(), input_art_prefs)
